bf86d39c1ef04ece1d9e66a1de8057b1796bace8
KiranMantha <kiranv.mantha@gmail.com>
2025-05-16 15:57:32 +0530

    fixed build and added prettier & vscode settings

---
+++ apps/web/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app';
+
+import localFont from 'next/font/local';
+import '../styles/globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans'
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono'
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
