3db34b4be7728df0f9c7a736af66f99b4de11585
KiranMantha <kiranv.mantha@gmail.com>
2025-06-04 11:24:33 +0530

    added webpack assets for aem integration

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,33 @@
 import { NextConfig } from 'next';
+import { WebpackAssetsManifest } from 'webpack-assets-manifest';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui'],
+  reactStrictMode: true,
   distDir: 'dist',
-  output: 'standalone'
+  output: 'standalone',
+  webpack(config) {
+    config.plugins.push(
+      new WebpackAssetsManifest({
+        output: '../public/asset-manifest.json',
+        transform: (assets) => {
+          const entrypoints = [];
+          for (const file in assets) {
+            if (file.includes('server/')) {
+              delete assets[file];
+            } else if ((assets[file] as string).endsWith('.js') || (assets[file] as string).endsWith('.css')) {
+              entrypoints.push(assets[file]);
+            }
+          }
+          return {
+            files: assets,
+            entrypoints: entrypoints
+          };
+        }
+      })
+    );
+    return config;
+  }
 };
 
 export default nextConfig;
